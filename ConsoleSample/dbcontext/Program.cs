861f9082d95a6d4f60fa58eecde42978ffbf9f0a
boringfish <boringfish@qq.com>
2024-07-06 16:00:50 +0800

    新增文件

--- ConsoleSample/dbcontext/Program.cs
+++ ConsoleSample/dbcontext/Program.cs
@@ -1,54 +1,112 @@
-﻿using SqlSugar;
+﻿using Microsoft.Extensions.DependencyInjection;
+using SqlSugar;
 
 
-//创建数据库对象 (用法和EF Dappper一样通过new保证线程安全)
-SqlSugarClient client = new SqlSugarClient(new ConnectionConfig()
+var service = new ServiceCollection();
+
+service.AddScoped<ISqlSugarClient>(s =>
 {
-    ConnectionString = "datasource=demo.db",
-    DbType = DbType.Sqlite,
-    IsAutoCloseConnection = true
-},
-db => {
- 
-    db.Aop.OnLogExecuting = (sql, pars) =>
+    //创建数据库对象 (用法和EF Dappper一样通过new保证线程安全)
+    SqlSugarClient client = new SqlSugarClient(new ConnectionConfig()
+    {
+        ConnectionString = "datasource=demo.db",
+        DbType = DbType.Sqlite,
+        IsAutoCloseConnection = true
+    },
+    db =>
     {
 
-        //获取原生SQL推荐 5.1.4.63  性能OK
-        Console.WriteLine(UtilMethods.GetNativeSql(sql, pars));
+        db.Aop.OnLogExecuting = (sql, pars) =>
+        {
 
-        //获取无参数化SQL 对性能有影响，特别大的SQL参数多的，调试使用
-        //Console.WriteLine(UtilMethods.GetSqlString(DbType.SqlServer, sql, pars));
+            //获取原生SQL推荐 5.1.4.63  性能OK
+            Console.WriteLine(UtilMethods.GetNativeSql(sql, pars));
+
+            //获取无参数化SQL 对性能有影响，特别大的SQL参数多的，调试使用
+            //Console.WriteLine(UtilMethods.GetSqlString(DbType.SqlServer, sql, pars));
 
 
-    };
+        };
 
-    //注意多租户 有几个设置几个
-    //db.GetConnection(i).Aop
+        //注意多租户 有几个设置几个
+        //db.GetConnection(i).Aop
 
+    });
+    return client;
 });
+service.AddScoped(typeof(SimpleClient<>));
 
+var provider = service.BuildServiceProvider();
+
+var client = provider.GetService<ISqlSugarClient>();
 //建库
 client.DbMaintenance.CreateDatabase();//达梦和Oracle不支持建库
 
 //建表（看文档迁移）
 client.CodeFirst.InitTables<Student>(); //所有库都支持     
 
-//查询表的所有
-var list = client.Queryable<Student>().ToList();
+if (false)
+{
+    //查询表的所有
+    var list = client.Queryable<Student>().ToList();
 
-//插入
-client.Insertable(new Student() { SchoolId = 1, Name = "jack" }).ExecuteCommand();
+    //插入
+    client.Insertable(new Student() { SchoolId = 1, Name = "jack" }).ExecuteCommand();
 
-//更新
-client.Updateable(new Student() { Id = 1, SchoolId = 2, Name = "jack2" }).ExecuteCommand();
+    //更新
+    client.Updateable(new Student() { Id = 1, SchoolId = 2, Name = "jack2" }).ExecuteCommand();
 
-//删除
-client.Deleteable<Student>().Where(it => it.Id == 1).ExecuteCommand();
+    //删除
+    client.Deleteable<Student>().Where(it => it.Id == 1).ExecuteCommand();
+}
+if (false)
+{
+    try
+    {
+        using (var uow = client.CreateContext())
+        {
+            //使用仓储查询
+            var repo = client.GetSimpleClient<Student>();
+            var rets = repo.AsQueryable().Where(it => it.Name == "jack2").ToList();
+            Console.WriteLine("执行前：" + rets.Count);
+
+            repo.Delete(rets);
+            rets = repo.AsQueryable().Where(it => it.Name == "jack2").ToList();
+            Console.WriteLine("执行后：" + rets.Count);
+
+            throw new Exception("抛出异常");
+        }
+    }
+    catch
+    {
+        //使用仓储查询
+        var repo = client.GetSimpleClient<Student>();
+        var rets = repo.AsQueryable().Where(it => it.Name == "jack2").ToList();
+        Console.WriteLine("回滚后：" + rets.Count);
+    }
+}
+
+{
+    client.Ado.BeginTran();
+    //使用仓储查询
+    var repo = client.GetSimpleClient<Student>();
+    var rets = repo.AsQueryable().Where(it => it.Name == "jack2").ToList();
+    Console.WriteLine("执行前：" + rets.Count);
+    repo.Delete(rets);
+    rets = repo.AsQueryable().Where(it => it.Name == "jack2").ToList();
+    Console.WriteLine("执行后：" + rets.Count);
+
+    client = provider.GetService<ISqlSugarClient>();
+    client.Ado.RollbackTran();
+    //使用仓储查询
+    repo = client.GetSimpleClient<Student>();
+    rets = repo.AsQueryable().Where(it => it.Name == "jack2").ToList();
+    Console.WriteLine("回滚后：" + rets.Count);
+}
 
 
-Console.WriteLine("恭喜你已经入门了,后面只需要用到什么查文档就可以了。");
 Console.ReadKey();
- 
+
 
 //实体与数据库结构一样
 public class Student
